a42f3cc78e39920a6e4122ffd2cd26dcfb81b4a5
accio-uday <udaymanideep2002@gmail.com>
2025-07-12 15:11:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+document.getElementById('ageForm').addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	const age = document.getElementById('age').value;
+	const name = document.getElementById('name').value;
+
+
+	if (!age || !name) {
+		alert("Please enter valid details.");
+		return;
+	}
+
+
+	const ageValidationPromise = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if (age >= 18) {
+				resolve(`Welcome, ${name}. You can vote.`);
+			} else {
+				reject(`Oh sorry ${name}. You aren't old enough.`);
+			}
+		}, 4000);
+	});
+
+
+	ageValidationPromise
+	.then(message => alert(message))
+	.catch(message => alert(message));
+});
